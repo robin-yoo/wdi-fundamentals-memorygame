e9a30e68d8916420bc11ca5dbf1c4e379d4f1f80
Robin Yoo <robinyoo25@gmail.com>
2018-11-24 13:39:37 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,21 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[1];
-var cardTwo = cards[0];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert('You found a match!');
-  } else {
-    alert('Sorry, try again.')
+var checkForMatch = function() {
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert('You found a match!');
+    } else {
+      alert('Sorry, try again.')
+    }
   }
 }
 
-console.log('User flipped ' + cardOne);
-console.log('User flipped ' + cardTwo);
+var flipCard = function(cardId) {
+  console.log('User flipped ' + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  return checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
